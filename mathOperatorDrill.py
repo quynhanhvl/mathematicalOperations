c7118ab79d2862a92426c876468916b9d64c1197
quynhanhvl <quynhanhvl@users.noreply.github.com>
2019-10-05 10:31:33 -0500

    Initial commit

---
+++ mathOperatorDrill.py
@@ -0,0 +1,104 @@
+#addition
+witch = 4
+
+spider = 5
+
+spooky = witch + spider
+print(spooky)
+
+a = 90
+
+b = 80
+c = a + b
+print(c)
+
+
+#subtraction
+
+q = 100000
+
+c = 2000
+
+difference = q - c
+print(difference) 
+
+d = 7
+
+r = 6
+
+diff = d - r
+print(diff) 
+
+
+#Multiplication
+
+x = 51
+
+y=3
+
+z = x * y
+print(z)
+
+t = 537
+
+c = 869
+
+p = t * c
+print(p)
+
+
+#division
+
+g = 8
+
+l = 2
+
+o = l / g
+print(o)
+
+j= 6
+
+k=1000000
+
+b = j / k
+print(b)
+
+
+#floor division
+
+i = 8
+
+w = 9
+
+z = i//w 
+print(z)
+
+f = 12
+
+q = 17
+
+w = f//q
+print(w)
+
+
+#modulus
+
+pie = 8
+
+purple = 6
+
+green = pie % purple
+print("green = " + str(green))
+
+learn = 6
+
+ostrich = 4
+
+flag = learn % ostrich
+print("flag = " + str(flag))
+
+
+
+
+
+
---
+++ practiceMathematicalOperation.py
@@ -0,0 +1,52 @@
+'''
+Created on Sep 28, 2019
+
+@author: ITAUser
+'''
+a = 2+3
+
+a = 2
+
+b = 2
+
+c = a + b
+
+c = b - a
+
+b ="quynhanh"
+c ="is amazing"
+
+d = b + " " + c 
+print(d)
+
+x = 2
+
+y = 3
+
+z = x * y 
+print(z)
+
+z = x / y
+print(z)
+
+z = x // y
+print(z)
+
+d = 20
+e = 6
+
+f = d//e
+print("f equals " + str(f))
+
+f =d % e
+print("f remainder " + str(f))
+
+
+
+
+
+
+
+
+
+

52d132f2524d0d100dbf4374b6b3f31dfe8ae48f
quynhanhvl <quynhanhvl@users.noreply.github.com>
2019-10-19 10:21:41 -0500

    Create mathOperatorsDebug.py

---
+++ mathOperatorsDebug.py
@@ -0,0 +1,35 @@
+#a.)
+40 * 0 == -1     
+
+# 40 times 0 is not equal to negative one; delete extra equal sign.
+
+#b.)
+“Hello 1 2 and” + 3
+
+#
+
+#c.)    
+6 _5 _1= 31
+
+# blank1 = * ; blank2 = + ;
+
+#d.)    
+
+print(10 + “/” + 2) = ?
+
+
+
+#e.)        
+print(“LOL” * 3) ? 
+
+# get rid of quotation marks around 'lol'
+
+#f.)       
+Print( “1” + “2” + “3”) 
+
+#
+
+#g.)   
+print(10 _ 2 == 0)    
+
+# delete extra equal; blank = %;
